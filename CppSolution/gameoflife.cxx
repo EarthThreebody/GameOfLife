cd28bddb1c6cc69a829189291e051310a4de9a45
Mincong <mincong.zhang@hotmail.com>
2016-07-05 12:57:26 +0100

    Update gameoflife.cxx

--- CppSolution/gameoflife.cxx
+++ CppSolution/gameoflife.cxx
@@ -30,28 +30,24 @@ inline bool lex_cast(const INPUT & in, OUTPUT & out){
   return true;
 }
 
-void printGrid(std::vector< std::vector<bool> > & grids){
-  for(size_t col=0; col<grids.size(); ++col){
-    for(size_t row=0; row<grids[col].size(); ++row){
-      if(grids[col][row]){
-        if(row == grids[col].size()-1)
-          std::cout<<"*";
-        else
-          std::cout<<"* ";
+void printGrid(std::vector< std::vector<bool> > & grid){
+  for(size_t col=0; col<grid.size(); ++col){
+    for(size_t row=0; row<grid[col].size(); ++row){
+      if(grid[col][row]){
+        std::cout<<ALIVE;
+        if(row != grid[col].size()-1){ std::cout<<" "; }
       } else {
-        if(row == grids[col].size()-1)
-          std::cout<<" ";
-        else
-          std::cout<<"  ";
+        std::cout<<DEAD;
+        if(row != grid[col].size()-1){ std::cout<<" "; }
       }
     }
     std::cout<<std::endl;
   }
 }
 
-int aliveNeighbour(const std::vector< std::vector<bool> > & grids, const size_t col,const size_t row){
+int aliveNeighbour(const std::vector< std::vector<bool> > & grid, const size_t col,const size_t row){
   int alive_num = 0;
-  size_t size_end = grids.size()-1;
+  size_t size_end = grid.size()-1;
   size_t up,low,left,right;
   if(row == 0)       {left = size_end;}
   else               {left = row-1;}
@@ -65,52 +61,54 @@ int aliveNeighbour(const std::vector< std::vector<bool> > & grids, const size_t 
   if(col == size_end){low = 0;}
   else               {low = col+1;}
 
-  if(grids[up][row]) alive_num++;
-  if(grids[low][row]) alive_num++;
+  if(grid[up][row]) alive_num++;
+  if(grid[low][row]) alive_num++;
 
-  if(grids[up][left]) alive_num++;
-  if(grids[up][right]) alive_num++;
+  if(grid[up][left]) alive_num++;
+  if(grid[up][right]) alive_num++;
 
-  if(grids[low][left]) alive_num++;
-  if(grids[low][right]) alive_num++;
+  if(grid[low][left]) alive_num++;
+  if(grid[low][right]) alive_num++;
 
-  if(grids[col][left]) alive_num++;
-  if(grids[col][right]) alive_num++;
+  if(grid[col][left]) alive_num++;
+  if(grid[col][right]) alive_num++;
 
   return alive_num;
 }
 
-void evolve(std::vector< std::vector<bool> > & grids, int & iterations,bool print=false){
+void evolve(std::vector< std::vector<bool> > & grid, int & iterations,bool print=false){
   if(print){
     log("Iterations:["<<iterations<<"]");
-    printGrid(grids);
+    printGrid(grid);
     std::cout << "Press enter to continue ...";
     std::cin.get();
   }
 
   while(iterations--){
-    std::vector< std::vector<bool> > new_grids = grids;
+    std::vector< std::vector<bool> > new_grid = grid;
+
     //for each cell
-    for(size_t col=0; col<grids.size(); ++col){
-      for(size_t row=0; row<grids[col].size(); ++row){
+    for(size_t col=0; col<grid.size(); ++col){
+      for(size_t row=0; row<grid[col].size(); ++row){
         //8 directions
-        int alive_num = aliveNeighbour(grids,col,row);
-        if(grids[col][row]){
+        int alive_num = aliveNeighbour(grid,col,row);
+        if(grid[col][row]){
           //For Alive cell
-          if     (alive_num<2) new_grids[col][row] = false;
-          else if(alive_num>3) new_grids[col][row] = false;
+          if     (alive_num<2) new_grid[col][row] = false;
+          else if(alive_num>3) new_grid[col][row] = false;
         }
         else {
           //For Dead cell
-          if(alive_num == 3)  new_grids[col][row] = true;
+          if(alive_num == 3)  new_grid[col][row] = true;
         }
       }//for row
     }//for col
-    grids = new_grids;
+
+    grid = new_grid;
 
     if(print){
       log("Iterations:["<<iterations<<"]");
-      printGrid(grids);
+      printGrid(grid);
       boost::this_thread::sleep(boost::posix_time::milliseconds(500));
       system("clear");
     }
@@ -120,12 +118,13 @@ void evolve(std::vector< std::vector<bool> > & grids, int & iterations,bool prin
 
 int main(int argc,  char* argv[]){
   if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " [filename] [true/false(print generations)]" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [filename] [(print generations)true/false]" << std::endl;
+    std::cerr << "e.g. ./bin/gameoflife high_iter.txt false" << std::endl;
     return 1;
   }
+
   std::string filename = argv[1];
   std::ifstream infile(filename.c_str());
-
 
   std::string print = argv[2];
   bool print_generations = false;
@@ -134,46 +133,53 @@ int main(int argc,  char* argv[]){
   }
   int size       = 0;
   int iterations = 0;
-  std::vector< std::vector<bool> > grids;
+  std::vector< std::vector<bool> > grid;
 
   std::string line;
   int count = 1;
   while (std::getline(infile, line)){
 
+    ///////////////////////////
     //Read size and iterations
+    ///////////////////////////
     if(size == 0 && lex_cast(line,size)){
-      std::vector<bool> grids_row(size,false);
-      grids.clear();
-      grids.resize(size,grids_row);
+      std::vector<bool> grid_row(size,false);
+      grid.clear();
+      grid.resize(size,grid_row);
       continue;
     }
     if(iterations <= 0 && lex_cast(line,iterations)){ continue; }
 
-    //Read Grids
-    std::vector<std::string> cells_row;
-    boost::split(cells_row,line,boost::is_any_of(" "));
-    for(size_t it=0; it<cells_row.size();++it){
-      //std::cout<<cells_row[it]<<" ";
-      std::vector<bool> & grids_row_ref = grids[grids.size()-size];
+    ///////////////////////////
+    //Read Grid
+    ///////////////////////////
+    std::vector<std::string> row;
+    boost::split(row,line,boost::is_any_of(" "));
+    for(size_t it=0; it<row.size();++it){
+      //std::cout<<row[it]<<" ";
+      std::vector<bool> & row_ref = grid[grid.size()-size];
 
-
-      if(cells_row[it]==ALIVE){
-        grids_row_ref[it] = true;
+      if(row[it]==ALIVE){
+        row_ref[it] = true;
         //std::cout<<"* ";
-      } else {
-        //std::cout<<"- ";
       }
+      // else {
+      //std::cout<<"- ";
+      //}
     }
     //std::cout<<std::endl;
 
     size--;
-    //Finish reading 1 set of grids, game starts
+
+    ////////////////////////////////////////////
+    //Finish reading 1 set of grid, game starts
+    ////////////////////////////////////////////
     if(size == 0){
       std::cout<<count<<std::endl;
-      evolve(grids,iterations,print_generations);
-      printGrid(grids);
+      evolve(grid,iterations,print_generations);
+      printGrid(grid);
       count++;
     }
   }
 
-}
+}//main
